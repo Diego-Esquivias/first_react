1ef3b391298714e834ac956d902a0bd07e5d8795
Diego-Esquivias <desqui048@west-mec.org>
2024-11-14 15:16:07 -0700

    Notes

---
+++ src/test.jsx
@@ -0,0 +1,21 @@
+import { GiCrackedAlienSkull } from "react-icons/gi";
+import Question from "./Question";
+
+function Test(){
+    const sports = "football"
+    let src = "https://i.pinimg.com/474x/e0/ca/79/e0ca7913e7b45d4f82de98e17ebc0407.jpg"
+    let w="300", h="500", alt="moon knight"
+    let info = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic sed ducimus dolorum similique cumque, suscipit error sapiente incidunt tempore blanditiis!"
+    return(
+    <>
+        <h1>Diego Esquivias</h1>
+        <p>Hello, my name is Diego Esquivias</p>
+        <h3>{sports}</h3>
+        <img src={src} width={w} height={h} alt={alt} />
+        <button><GiCrackedAlienSkull size={300} style={{backgroundColor: "red", color: "black"}}/></button>
+        <Question title="Murders only happen in the building" info={info}/>
+    </>
+    )
+}
+
+export default Test;
