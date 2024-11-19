30cf5fac50221755c798efdfc98a2d8b63cb483b
Diego-Esquivias <desqui048@west-mec.org>
2024-11-19 15:16:33 -0700

    notes

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+// import {useState} from 'react'
+
+const Button = ({count, onClick}) => {
+  return (
+    <button onClick={onClick}>
+        Total # of Clicks {count}
+    </button>
+  )
+}
+
+export default Button
--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,6 +1,9 @@
 import { GiCrackedAlienSkull } from "react-icons/gi";
-import Question from "../components/Question";
-import ProfileCard from "../components/ProfileCard"
+import {default as Topics} from "../components/Question";
+// import ProfileCard from "../components/ProfileCard"
+import {useState} from 'react'
+import React from "react";
+import Button from "../components/Button";
 
 function Test(){
     const sports = "football"
@@ -8,22 +11,32 @@ function Test(){
     let w="300", h="500", alt="moon knight"
     let info = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic sed ducimus dolorum similique cumque, suscipit error sapiente incidunt tempore blanditiis!"
     let triggered = [28, 56, 19]
+    
+    const [count, setCount] = useState(0)
+    const handleClick = () => {
+        setCount(count + 1)
+    }
+
     return(
     <>
+        <Button count={count} onClick={handleClick}/>
+        <Button count={count} onClick={handleClick}/>
+        <h1>Hello World!</h1>
         <h1>Diego Esquivias</h1>
         <p>Hello, my name is Diego Esquivias</p>
         <h3>{sports}</h3>
         <img src={src} width={w} height={h} alt={alt} />
-        <button><GiCrackedAlienSkull size={300} style={{backgroundColor: "red", color: "black"}}/></button>
-        <Question title="Murders only happen in the building" info={info}/>
+        <button><GiCrackedAlienSkull size={300} style={{backgroundColor: "black", color: "limeGreen"}}/></button>
+        <Topics title="Murders only happen in the building" info={info}/>
 
-        <ProfileCard profileImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmDwycVzCsWaZx5AaCTeqz6e8qLbt8UaQz7g&s" firstName="John" lastName="Doe" email="johndoe@example.com" socialLinks={{ github: "https://github.com/johndoe", instagram: "https://instagram.com/johndoe", facebook: "https://facebook.com/johndoe" }} />
 
         {
             triggered.length > 0 ? <h1>Hello Data</h1> : <h1>Nothing Found</h1>
         }
 
-
+        {/* <ProfileCard profileImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmDwycVzCsWaZx5AaCTeqz6e8qLbt8UaQz7g&s" firstName="John" lastName="Doe" email="johndoe@example.com" socialLinks={{ github: "https://github.com/johndoe", instagram: "https://instagram.com/johndoe", facebook: "https://facebook.com/johndoe" }} /> */}
+        <br />
+        <Button />
     </>
     )
 }
